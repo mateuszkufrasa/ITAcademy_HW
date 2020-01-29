aa3a03be81ec9b5a104e0f855a9bc48b3545803c
Alexey Mukas <alexey.mukas@gmail.com>
2020-01-29 20:01:32 +0100

    update

--- src/main/java/product_analyzer/domain/Product.java
+++ src/main/java/product_analyzer/domain/Product.java
@@ -1,29 +1,59 @@
 package product_analyzer.domain;
 
-public class Product {
+interface CanDescribe {
+    String describe();
+}
+
+interface WithPrice {
+    Double getPrice();
+}
+
+interface Label {
+    String toLabel(); // "<name> <price> <wight> <type>" e.g. "iPhone 200 0.2 KG"
+}
+
+public class Product implements CanDescribe, WithPrice { //todo: implement Label
     public String name; // laptop
     public Double price; // 1000
     public Double weight; // 2.0
     public WeightType weightType; // KG
 
-    //todo: [optional] constructor that has name
-}
-
-class Laptop extends Product {
-    private boolean hasSsd;
-
-    public Laptop(String name) {
-        this.name = name; //todo: [optional] call parent constructor
-    }
-
-    //todo: add a constructor that accepts name and hasSsd
-}
-
-class Phone extends Product {
-    public Phone(String name) {
+    public Product(String name) {
         this.name = name;
     }
+
+    @Override
+    public String describe() {
+        return "Product with name [" + name + "]";
+    }
+
+    @Override
+    public Double getPrice() {
+        return this.price;
+    }
 }
+
+class Laptop extends Product implements CanDescribe, WithPrice {
+    public  boolean hasSsd;
+
+    public Laptop(String name) {
+        super(name);
+    }
+}
+
+class App {
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop("Lenovo");
+        Product laptopAsAProduct = new Laptop("Lenovo");
+
+        CanDescribe describable = laptop;
+        describable.describe();
+
+        //todo: create a Label variable and assign new instance of Product, then use this variable to print to a console
+
+    }
+}
+
 
 class LaptopCollection {
     private Laptop laptop1;
@@ -63,63 +93,63 @@ class LaptopCollection {
                 '}';
     }
 }
-
-class PhoneCollection {
-    private Phone phone1;
-    private Phone phone2;
-    private Phone phone3;
-
-    public void set(int index, Phone phone) {
-        if (index == 1) {
-            this.phone1 = phone;
-        } else if (index == 2) {
-            this.phone2 = phone;
-        } else if (index == 3) {
-            this.phone3 = phone;
-        } else {
-            throw new RuntimeException("not supported index [" + index + "]");
-        }
-    }
-
-    public Phone get(int index) {
-        if (index == 1) {
-            return this.phone1;
-        } else if (index == 2) {
-            return this.phone2;
-        } else if (index == 3) {
-            return this.phone3;
-        } else {
-            throw new RuntimeException("not supported index [" + index + "]");
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "PhoneCollection{" +
-                "phone1=" + phone1.name +
-                ", phone2=" + phone1.name +
-                ", phone3=" + phone1.name +
-                '}';
-    }
-}
-
-//todo: create a generic version of PhoneCollection and LaptopCollection
-
-class MyApp2 {
-    public static void main(String[] args) {
-        //todo: use that generic class here
-
-        LaptopCollection laptops = new LaptopCollection();
-        laptops.set(1, new Laptop("Laptop #1"));
-        laptops.set(2, new Laptop("Laptop #2"));
-        laptops.set(3, new Laptop("Laptop #3"));
-        System.out.println(laptops);
-
-        PhoneCollection phones = new PhoneCollection();
-        phones.set(1, new Phone("Laptop #1"));
-        phones.set(2, new Phone("Laptop #2"));
-        phones.set(3, new Phone("Laptop #3"));
-        System.out.println(phones);
-
-    }
-}
+//
+//class PhoneCollection {
+//    private Phone phone1;
+//    private Phone phone2;
+//    private Phone phone3;
+//
+//    public void set(int index, Phone phone) {
+//        if (index == 1) {
+//            this.phone1 = phone;
+//        } else if (index == 2) {
+//            this.phone2 = phone;
+//        } else if (index == 3) {
+//            this.phone3 = phone;
+//        } else {
+//            throw new RuntimeException("not supported index [" + index + "]");
+//        }
+//    }
+//
+//    public Phone get(int index) {
+//        if (index == 1) {
+//            return this.phone1;
+//        } else if (index == 2) {
+//            return this.phone2;
+//        } else if (index == 3) {
+//            return this.phone3;
+//        } else {
+//            throw new RuntimeException("not supported index [" + index + "]");
+//        }
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "PhoneCollection{" +
+//                "phone1=" + phone1.name +
+//                ", phone2=" + phone1.name +
+//                ", phone3=" + phone1.name +
+//                '}';
+//    }
+//}
+//
+////todo: create a generic version of PhoneCollection and LaptopCollection
+//
+//class MyApp2 {
+//    public static void main(String[] args) {
+//        //todo: use that generic class here
+//
+//        LaptopCollection laptops = new LaptopCollection();
+//        laptops.set(1, new Laptop("Laptop #1"));
+//        laptops.set(2, new Laptop("Laptop #2"));
+//        laptops.set(3, new Laptop("Laptop #3"));
+//        System.out.println(laptops);
+//
+//        PhoneCollection phones = new PhoneCollection();
+//        phones.set(1, new Phone("Laptop #1"));
+//        phones.set(2, new Phone("Laptop #2"));
+//        phones.set(3, new Phone("Laptop #3"));
+//        System.out.println(phones);
+//
+//    }
+//}
