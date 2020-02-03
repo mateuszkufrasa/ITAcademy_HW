b8458e4e922db95f3d98afcf871820626c61fd2c
Mateusz Kufrasa <mateusz.kufrasa@gmail.com>
2020-02-03 22:57:39 +0100

    1000 laptops added to an array list and printed

--- src/main/java/product_analyzer/ProductAnalyzerApp.java
+++ src/main/java/product_analyzer/ProductAnalyzerApp.java
@@ -22,7 +22,7 @@ public class ProductAnalyzerApp {
         }
         //todo: task2 generate collection of Phones(1_000 items). It doesn't exist, you need to create it. Use ProductCollection class as storage.
         //      you can modify any class in product_analyzer.domain package if you need.
-//dsvdsv;
+
         //todo: task3[optional] generate collections in task1 and task2 in thread, generate 10_000 items in total, you need to create 10 threads, each you generate 1_000 items.
         //      make sure the result is correct, fix bug is there will be a need.
     }
